fa209999e52ff8880cdb210bb3552b6153c5a61d
“naidus199” <naidus199@gmail.com>
2023-08-08 21:49:42 +0530

    some changes

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -34,8 +34,7 @@ class DataTransformation:
                 "race_ethnicity",
                 "parental_level_of_education",
                 "lunch",
-                "test_preparation_course",
-            ]
+                "test_preparation_course"]
 
             num_pipeline= Pipeline(
                 steps=[
